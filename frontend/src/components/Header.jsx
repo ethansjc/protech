fa8aac095e72b55d62751fb2d9325a452bc2e033
ethansjc <ethansjcheong@gmail.com>
2023-06-13 16:52:01 -0400

    Added asset imports

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
-import {FaShoppingCart, FaUser} from 'react-icons/fa'
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+
 
 const Header = () => {
   return (
