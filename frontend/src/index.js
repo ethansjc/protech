740bbe12c4f875a472c58b6b2d319a5a385f6b33
ethansjc <ethansjcheong@gmail.com>
2023-08-24 15:04:01 -0400

    Redux Store & API Slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,10 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-RouterProvider} from 'react-router-dom'
+  RouterProvider
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/index.css';
 import App from './App';
@@ -23,6 +26,8 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
