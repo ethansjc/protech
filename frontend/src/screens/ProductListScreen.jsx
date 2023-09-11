55398adbf88357026af38a9123a02b662447f07e
ethansjc <ethansjcheong@gmail.com>
2023-09-10 23:18:14 -0400

    Image Upload Endpoint

--- frontend/src/screens/ProductListScreen.jsx
+++ frontend/src/screens/ProductListScreen.jsx
@@ -68,7 +68,7 @@ const ProductListScreen = () => {
                 <tr key={product._id}>
                   <td>{product._id}</td>
                   <td>{product.name}</td>
-                  <td>{product.price}</td>
+                  <td>${product.price}</td>
                   <td>{product.category}</td>
                   <td>{product.brand}</td>
                   <td>
